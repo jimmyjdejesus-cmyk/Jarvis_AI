50aa13bd8808abf689dfa9f171c77396de51561d
Jimmy-Dejesus <jimmy@bravetto.com>
2025-12-17 08:05:05 -0500

    chore: Remove temporary test file

--- test_import.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-sys.path.insert(0, '.')
-try:
-    from adaptivemind_core.app import AdaptiveMindApplication
-    print('Import successful')
-except Exception as e:
-    print(f'Import failed: {e}')
-
